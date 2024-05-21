b39774d3217edf206eebd338205fac72ce1524bc
black <jaberhc2002@gmail.com>
2024-05-21 03:49:35 +0000

    init : starter branch

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-import "./style.css";
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
 
-window.onload = () => {
-  const numberTag = document.querySelector(".number");
-  
-  const generateNumbers = () => {
-    let newArray = [];
-    for (let i = 0; i < window.innerHeight; i++) {
-      newArray.push(Math.floor(Math.random() * 9));
-    }
-    return newArray;
-  };
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
 
-  // add numbers inside numberTag
-  const addNumbers = () => {
-    const numbers = generateNumbers();
-    let newElements = "";
-    for (let i = 0; i < numbers.length; i++) {
-      newElements += "<span>" + numbers[i] + "</span>";
-    }
-    numberTag.innerHTML = newElements;
-  };
-
-  addNumbers();
-};
+setupCounter(document.querySelector('#counter'))
