324a5a1ff4fcee8a7d30895858dd168831a2a7c1
Black Light <78747748+JaberChowdhury@users.noreply.github.com>
2025-01-23 01:49:27 +0600

    update : enhance index.html layout with grid structure and improve styling; modify package.json for Vite host support; clean up main.ts; add grid styles in style.css

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import "./style.css";
 
-window.onload = () => {
-  console.log("Hello World");
-};
+// window.onload = () => {
+//   const root = document.getElementById("root");
+//   const html_data = [];
+//   for (let i = 0; i < 100; i++) {
+//     html_data.push(`<div class="border border-slate-700 rounded p-2">
+//         <p class="text-3xl text-center">Lorem, ipsum</p>
+//         <p class="text-center my-4">
+//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus a
+//           error eveniet hic? Ducimus, adipisci.
+//         </p>
+//       </div>`);
+//   }
+//   if (root) {
+//     root.innerHTML = html_data.join("");
+//   }
+//   console.log("Hello World");
+// };
