1cdabcc3b8825760b657f141a02639f3db8faece
Black Light <78747748+JaberChowdhury@users.noreply.github.com>
2025-01-31 00:07:55 +0600

    add : integrate DaisyUI for enhanced styling and update HTML structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 };
