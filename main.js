82d15e9e584fc8ca156ccf6513a971b2c6659ef5
Black Light <78747748+JaberChowdhury@users.noreply.github.com>
2025-03-06 22:44:56 +0600

    add : css

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 import "./style.css";
 import data from "./minidata.json";
+// import data from "./data.json";
 
 window.onload = () => {
   const parsedData = JSON.parse(JSON.stringify(data));
   const dataArray = Object.entries(parsedData);
+
+  const color = () => {
+    const r = Math.floor(Math.random() * 9);
+    const g = Math.floor(Math.random() * 9);
+    const b = Math.floor(Math.random() * 9);
+    return `#${r}${g}${b}${r}${g}${b}`;
+  };
 
   const render_data = (eachData) => {
     const domtag = document.createElement("div");
@@ -12,22 +20,23 @@ window.onload = () => {
       const key = eachData[i][0];
       const value = eachData[i][1];
 
-      const keyElement = document.createElement("strong");
+      const keyElement = document.createElement("div");
       keyElement.textContent = key + ": ";
+      keyElement.classList.add("key");
       domtag.appendChild(keyElement);
 
       if (typeof value === "string" || typeof value === "number") {
         const valueElement = document.createElement("span");
         valueElement.textContent = value;
-        valueElement.setAttribute("class", "list-row border border-black");
+        valueElement.setAttribute("class", `box bg-[${color()}]/[0.52]`);
         domtag.appendChild(valueElement);
       } else if (typeof value === "object" && value !== null) {
         const nestedContainer = render_data(Object.entries(value));
-        nestedContainer.setAttribute("class", "list-row");
+        nestedContainer.setAttribute("class", "box");
         domtag.appendChild(nestedContainer);
       } else if (typeof value === "array" && value !== null) {
         const nestedContainer = render_data(data);
-        nestedContainer.setAttribute("class", "list-row");
+        nestedContainer.setAttribute("class", "box");
         domtag.appendChild(nestedContainer);
       }
 
